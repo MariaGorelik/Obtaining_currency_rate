837f95529ce7541d338c21e93bea70e2c6633343
Maria Gorelik <marya.gorelik@gmail.com>
2024-06-01 19:47:44 +0300

    add endpoints

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
 import requests
 
 app = Flask(__name__)
@@ -6,17 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    cur_id = 440
-    target_url = f"https://api.nbrb.by/exrates/rates/{cur_id}"
-
-    params = {'ondate': '2024-05-29', 'periodicity': 0}
-
-    try:
-        response = requests.get(target_url, params=params)
-        response.raise_for_status()  # Проверка на ошибки HTTP
-        return jsonify(response.json())
-    except requests.exceptions.RequestException as e:
-        return jsonify({'error': str(e)}), 500
+    pass
 
 
 @app.route('/<date>/') # change url
@@ -25,7 +15,7 @@ def upload_currency_information(date):
     params = {'ondate': date, 'periodicity': 0}
     try:
         response = requests.get(target_url, params=params)
-        response.raise_for_status()  # Проверка на ошибки HTTP
+        response.raise_for_status()
         return jsonify(response.json())
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
@@ -37,7 +27,7 @@ def get_currency_information(date, cur_id):
     params = {'ondate': date, 'periodicity': 0, 'parammode': 2}
     try:
         response = requests.get(target_url, params=params)
-        response.raise_for_status()  # Проверка на ошибки HTTP
+        response.raise_for_status() 
         return jsonify(response.json())
     except requests.exceptions.RequestException as e:
         return jsonify({'error': str(e)}), 500
